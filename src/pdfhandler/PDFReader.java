9efab81f83b835639530ed157f176ba6eef902bc
Acer <Acer@acer.lan>
2019-05-28 20:51:50 +0200

    First version

---
+++ src/pdfhandler/PDFReader.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdfhandler;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PDFReader {
+
+    public PDFReader(String path) throws IOException {
+
+        try (PDDocument document = PDDocument.load(new File(path))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+			
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+				// split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+
+        }
+
+    }
+}
